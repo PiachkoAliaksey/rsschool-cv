33cb2bd10cda56927fb19cf3092b1d5db7b9c052
PiachkoAliaksey <alek17y@mail.ru>
2023-02-21 23:09:51 +0300

    refactor: files

--- index.js
+++
@@ -1,8 +0,0 @@
-const navOpen=document.querySelector('.nav__list')
-const toggleBurger=document.querySelector('.burger input')
-
-
-
-toggleBurger.addEventListener('click',()=>{
-    navOpen.classList.toggle('nav__list_open');
-})
